8c70daaa77437453212f7d16a03fe1ab26afa8ae
samaoff <samaoff@gmail.com>
2024-11-08 23:19:57 +0100

    update

--- updateproyecto.bat
+++ updateproyecto.bat
@@ -4,4 +4,12 @@ git pull origin main
 echo "Instalando dependencias del proyecto..."
 npm install
 
+
+echo "Genera el Cliente de Prisma..."
+npx prisma generate
+
+echo "Si la base de datos giupos.db está vacía o no tiene la estructura esperada, ejecuta las migraciones de Prisma para crear las tablas necesarias.."
+npx prisma migrate dev
+
+
 echo "¡El proyecto está actualizado y listo para trabajar!"
